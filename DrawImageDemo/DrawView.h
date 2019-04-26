728ba52022a572fd01e15fdea001eee477ff6af3
黄洪彬 <huanghongbin@corp.netease.com>
2019-04-26 17:40:06 +0800

    first commit

---
+++ DrawImageDemo/DrawView.h
@@ -0,0 +1,17 @@
+//
+//  DrawView.h
+//  DrawImageDemo
+//
+//  Created by 黄洪彬 on 2019/4/26.
+//  Copyright © 2019 netease. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DrawView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ DrawImageDemo/DrawView.m
@@ -0,0 +1,21 @@
+//
+//  DrawView.m
+//  DrawImageDemo
+//
+//  Created by 黄洪彬 on 2019/4/26.
+//  Copyright © 2019 netease. All rights reserved.
+//
+
+#import "DrawView.h"
+
+@implementation DrawView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
